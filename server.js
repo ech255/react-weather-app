edd2d38a769753db47d9a0bb582e2e1368fe7a49
haynes89 <haynesca@oregonstate.edu>
2020-10-27 14:37:11 -0400

    heroku changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
 if (process.env.NODE_ENV === 'production') {
         // Exprees will serve up production assets
         app.use(express.static('client/build'));
-        app.get('*', (req, res) => {
+        app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
